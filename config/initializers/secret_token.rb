fa75ef1f32176b637251f81d205869996c4684c8
Aline Ebone <ini.ebone@gmail.com>
2013-07-04 17:41:01 -0300

    Initializing...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abeira::Application.config.secret_token = '741c4a8419c1a518e8ef18e71cc7225251b2fd503e46034f64707206e98e4a37b7df0c649cc0bf747f16ca022e6ad098d2ad326cd51ad57f0372026449dbfcf2'
